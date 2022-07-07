e9c774987570b40bcf906ff1d57cf799c70c47c7
Karthikeya <80380800+karthikeyay@users.noreply.github.com>
2022-07-07 08:41:48 +0530

    icons css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header/Header'
 import '../styles/globals.scss'
+import '@mdi/font/css/materialdesignicons.min.css'
 import 'swiper/css'
 import Footer from "components/Footer/Footer"
 
