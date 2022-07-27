db0ed11a63c0c6107368c52bf7e8ee57839dc7ca
Karthikeya <80380800+karthikeyay@users.noreply.github.com>
2022-07-28 03:28:27 +0530

    data sync fix

--- pages/focus/[tab].js
+++ pages/focus/[tab].js
@@ -15,28 +15,26 @@ export default function FocusTab() {
     const router = useRouter();
     const { tab } = router.query;
 
-    const [isDataLoaded, setIsDataLoaded] = useState(false);
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        if (data) {
-            setIsDataLoaded(true);
-            console.log(data);
-        }
-    }, [data]);
+    const [industrialData, setIndustrialData] = useState(null);
+    const [designData, setDesignData] = useState(null);
+    const [emsData, setEmsData] = useState(null);
 
     useEffect(() => {
         if (!tab || !tabWhitelist.includes(tab.toLowerCase())) {
             router.push("/focus/" + tabWhitelist[0]);
         } else {
-            setData(null);
             fetchData();
         }
     }, [tab]);
 
     const fetchData = async () => {
         const { data } = await axios.get(`${API_URL}/${tab}?${ARG_POPULATE_DEEP}`);
-        setData(data && data.data && data.data.attributes ? data.data.attributes : null);
+        if (tab == 'industrial')
+            setIndustrialData(data && data.data && data.data.attributes ? data.data.attributes : null);
+        if (tab == 'design')
+            setDesignData(data && data.data && data.data.attributes ? data.data.attributes : null);
+        if (tab == 'ems')
+            setEmsData(data && data.data && data.data.attributes ? data.data.attributes : null);
     }
 
     return <div className={styles.FocusPage}>
@@ -48,17 +46,17 @@ export default function FocusTab() {
         </div>
 
         {/* Industrial */}
-        {tab == 'industrial' && data ? <div className={styles.Industrial}>
+        {tab == 'industrial' && industrialData ? <div className={styles.Industrial}>
             <div className="w-full relative">
                 <div className="absolute w-[70%] h-full right-10">
-                    <Image layout="fill" objectFit="contain" objectPosition="center" src={data?.content[0].image.data.attributes.url} />
+                    <Image layout="fill" objectFit="contain" objectPosition="center" src={industrialData?.content[0].image.data.attributes.url} />
                 </div>
                 <div className="contentContainer flex items-center bg-white-100" style={{ minHeight: '400px' }}>
                     <div className={styles.ContentOne}>
                         <span className="BrandDot"></span>
                         <span className="BrandDot"></span>
                         <h5>01</h5>
-                        <p dangerouslySetInnerHTML={{ __html: data?.content[0].para }}></p>
+                        <p dangerouslySetInnerHTML={{ __html: industrialData?.content[0].para }}></p>
                     </div>
                 </div>
             </div>
@@ -68,11 +66,11 @@ export default function FocusTab() {
                 </div>
                 <div className="contentContainer">
                     <div className="grid grid-cols-2 py-6">
-                        <div className="col-sapn-1 flex items-center" dangerouslySetInnerHTML={{ __html: data?.content[1].para }}>
+                        <div className="col-sapn-1 flex items-center" dangerouslySetInnerHTML={{ __html: industrialData?.content[1].para }}>
 
                         </div>
                         <div className="col-sapn-1 flex justify-end">
-                            <div className="relative z-10 top-10 shadow-md"><Image width={300} height={240} src={data?.content[1].image.data.attributes.url} /></div>
+                            <div className="relative z-10 top-10 shadow-md"><Image width={300} height={240} src={industrialData?.content[1].image.data.attributes.url} /></div>
                         </div>
                     </div>
                 </div>
@@ -81,22 +79,22 @@ export default function FocusTab() {
                 <div className="contentContainer">
                     <div className="grid grid-cols-2 py-10">
                         <div className="col-span-1 py-6 flex justify-center items-center">
-                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={data?.content[2].image.data.attributes.url} /></div>
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={industrialData?.content[2].image.data.attributes.url} /></div>
                         </div>
                         <div className="col-span-1 py-6 flex items-center">
-                            <div className={styles.ContentTwo}><p dangerouslySetInnerHTML={{ __html: data?.content[2].para }}></p><h5>02</h5></div>
+                            <div className={styles.ContentTwo}><p dangerouslySetInnerHTML={{ __html: industrialData?.content[2].para }}></p><h5>02</h5></div>
                         </div>
                         <div className="col-span-1 py-6 flex items-center">
-                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: data?.content[3].para }}></div>
+                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: industrialData?.content[3].para }}></div>
                         </div>
                         <div className="col-span-1 py-6 flex justify-center items-center">
-                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={data?.content[3].image.data.attributes.url} /></div>
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={industrialData?.content[3].image.data.attributes.url} /></div>
                         </div>
                         <div className="col-span-1 py-6 flex justify-center items-center">
-                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={data?.content[4].image.data.attributes.url} /></div>
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={industrialData?.content[4].image.data.attributes.url} /></div>
                         </div>
                         <div className="col-span-1 py-6 flex items-center">
-                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: data?.content[4].para }}></div>
+                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: industrialData?.content[4].para }}></div>
                         </div>
                     </div>
                 </div>
@@ -112,10 +110,10 @@ export default function FocusTab() {
                     </div>
                     <div className="grid grid-cols-2 px-12 py-8 bg-white rounded shadow-xl mb-6">
                         <div className="col-span-1 relative h-full text-sm flex items-center">
-                            <p dangerouslySetInnerHTML={{ __html: data?.qualityProgram?.content }}></p>
+                            <p dangerouslySetInnerHTML={{ __html: industrialData?.qualityProgram?.content }}></p>
                         </div>
                         <div className="col-span-1 relative h-full flex items-center justify-center">
-                            {data?.qualityProgram?.badges.map((badge, bi) => <div key={`badge${bi}`} className="flex flex-col text-center text-sm mx-3">
+                            {industrialData?.qualityProgram?.badges.map((badge, bi) => <div key={`badge${bi}`} className="flex flex-col text-center text-sm mx-3">
                                 <Image width={150} height={150} src={badge.image.data.attributes.url} objectFit="contain" />
                                 <span className="my-2">{badge.title}</span>
                             </div>)}
