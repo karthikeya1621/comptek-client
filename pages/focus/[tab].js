18f18bb0f97ef1c62918388e5ad0277597d270b3
Karthikeya <80380800+karthikeyay@users.noreply.github.com>
2022-08-20 05:04:56 +0530

    focus pages

--- pages/focus/[tab].js
+++ pages/focus/[tab].js
@@ -13,11 +13,11 @@ export const tabWhitelist = ['industrial', 'design', 'ems'];
 
 export default function FocusTab() {
     const router = useRouter();
-    const { tab } = router.query;
 
     const [industrialData, setIndustrialData] = useState(null);
     const [designData, setDesignData] = useState(null);
     const [emsData, setEmsData] = useState(null);
+    const [tab, setTab] = useState(tabWhitelist[0] || 'industrial');
 
     useEffect(() => {
         if (!tab || !tabWhitelist.includes(tab.toLowerCase())) {
@@ -27,6 +27,12 @@ export default function FocusTab() {
         }
     }, [tab]);
 
+    useEffect(() => {
+        if (router.query && router.query.tab) {
+            setTab(router.query.tab);
+        }
+    }, [router.query]);
+
     const fetchData = async () => {
         const { data } = await axios.get(`${API_URL}/${tab}?${ARG_POPULATE_DEEP}`);
         if (tab == 'industrial')
@@ -35,6 +41,8 @@ export default function FocusTab() {
             setDesignData(data && data.data && data.data.attributes ? data.data.attributes : null);
         if (tab == 'ems')
             setEmsData(data && data.data && data.data.attributes ? data.data.attributes : null);
+
+        console.log('Tab', tab);
     }
 
     return <div className={styles.FocusPage}>
@@ -118,6 +126,149 @@ export default function FocusTab() {
                                 <span className="my-2">{badge.title}</span>
                             </div>)}
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div> : <></>}
+
+        {/* Design */}
+        {tab == 'design' && designData ? <div className={styles.Design}>
+            <div className="w-full relative py-4">
+                <div className="w-full absolute h-full">
+                    <Image layout="fill" objectFit="cover" src={designData?.content[0].image.data.attributes.url} />
+                </div>
+                <div className="relative contentContainer">
+                    <div className={styles.ContentOne}>
+                        <span className="BrandDot"></span>
+                        <span className="BrandDot"></span>
+                        <h5>01</h5>
+                        <p dangerouslySetInnerHTML={{ __html: designData?.content[0].para }}></p>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full relative bg-brand text-white">
+                <div className="BrandBg">
+                    <Image src={bgImage1} layout="responsive" objectFit="contain" />
+                </div>
+                <div className="contentContainer">
+                    <div className="grid grid-cols-2 py-6">
+                        <div className="col-sapn-1 flex items-center" dangerouslySetInnerHTML={{ __html: designData?.content[1].para }}>
+
+                        </div>
+                        <div className="col-sapn-1 flex justify-end">
+                            <div className="relative z-10 top-10 shadow-md"><Image width={300} height={240} src={designData?.content[1].image.data.attributes.url} /></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full relative bg-gray-50">
+                <div className="contentContainer">
+                    <div className="grid grid-cols-2 py-10">
+                        <div className="col-span-1 py-6 flex justify-center items-center">
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={designData?.content[2].image.data.attributes.url} /></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex items-center">
+                            <div className={styles.ContentTwo}><p dangerouslySetInnerHTML={{ __html: designData?.content[2].para }}></p><h5>02</h5></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex items-center">
+                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: designData?.content[3].para }}></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex justify-center items-center">
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={designData?.content[3].image.data.attributes.url} /></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex justify-center items-center">
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={designData?.content[4].image.data.attributes.url} /></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex items-center">
+                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: designData?.content[4].para }}></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex items-center">
+                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: designData?.content[5].para }}></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex justify-center items-center">
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={designData?.content[5].image.data.attributes.url} /></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex justify-center items-center">
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={designData?.content[6].image.data.attributes.url} /></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex items-center">
+                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: designData?.content[6].para }}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full relative bg-brand text-white">
+                <div className="BrandBg _Left">
+                    <Image src={bgImage1} layout="responsive" objectFit="contain" />
+                </div>
+                <div className="contentContainer grid grid-cols-2 gap-3">
+                    <div className="col-span-1 px-4 py-3" dangerouslySetInnerHTML={{ __html: designData?.content[7].para }}></div>
+                    <div className="col-span-1 px-4 py-3" dangerouslySetInnerHTML={{ __html: designData?.content[8].para }}></div>
+                    <div className="col-span-1 px-4 py-3" dangerouslySetInnerHTML={{ __html: designData?.content[9].para }}></div>
+                    <div className="col-span-1 px-4 py-3" dangerouslySetInnerHTML={{ __html: designData?.content[10].para }}></div>
+                </div>
+            </div>
+        </div> : <></>}
+
+        {/* EMS */}
+        {tab == 'ems' && emsData ? <div className={styles.EMS}>
+            <div className="w-full relative">
+                <div className="absolute w-[70%] h-full right-10">
+                    <Image layout="fill" objectFit="contain" objectPosition="center" src={emsData?.content[0].image.data.attributes.url} />
+                </div>
+                <div className="contentContainer flex items-center bg-white-100" style={{ minHeight: '400px' }}>
+                    <div className={styles.ContentOne}>
+                        <span className="BrandDot"></span>
+                        <span className="BrandDot"></span>
+                        <h5>01</h5>
+                        <p dangerouslySetInnerHTML={{ __html: emsData?.content[0].para }}></p>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full relative bg-brand text-white">
+                <div className="BrandBg">
+                    <Image src={bgImage1} layout="responsive" objectFit="contain" />
+                </div>
+                <div className="contentContainer">
+                    <div className="grid grid-cols-2 py-6">
+                        <div className="col-sapn-1 flex items-center" dangerouslySetInnerHTML={{ __html: emsData?.content[1].para }}>
+
+                        </div>
+                        <div className="col-sapn-1 flex justify-end">
+                            <div className="relative z-10 top-10 shadow-md"><Image width={300} height={240} src={emsData?.content[1].image.data.attributes.url} /></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full relative bg-gray-50">
+                <div className="contentContainer">
+                    <div className="grid grid-cols-2 py-10">
+                        <div className="col-span-1 py-6 flex justify-center items-center">
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={emsData?.content[2].image.data.attributes.url} /></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex items-center">
+                            <div className={styles.ContentTwo}><p dangerouslySetInnerHTML={{ __html: emsData?.content[2].para }}></p><h5>02</h5></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex items-center">
+                            <div className={styles.ContentTwo} dangerouslySetInnerHTML={{ __html: emsData?.content[3].para }}></div>
+                        </div>
+                        <div className="col-span-1 py-6 flex justify-center items-center">
+                            <div className="shadow"><Image width={340} height={260} objectFit="cover" src={emsData?.content[3].image.data.attributes.url} /></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full relative bg-brand">
+                <div className="BrandBg _Left">
+                    <Image src={bgImage1} layout="responsive" objectFit="contain" />
+                </div>
+                <div className="contentContainer py-6">
+                    <div className="sectionCount my-6 mx-auto">
+                        <span className="sectionNumber text-white/30">03</span>
+                        <h2 className="text-white">EMS Process and Control</h2>
+                    </div>
+                    <div className="flex items-center justify-center px-12 py-8 mb-6 relative h-[600px]">
+                        <Image height={600} width={1000} src={'https://comptek-files.s3.ap-south-1.amazonaws.com/ems9_de6bcfdbac.png?updated_at=2022-07-25T10:26:21.060Z'} layout="fill" objectFit="contain"  />
                     </div>
                 </div>
             </div>
